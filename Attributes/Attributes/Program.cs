8589432eb7cc1cfbf1b2d63b10ffd0d16dd46f03
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 16:54:17 +0530

    Attributes

---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int result = Add(10, 20);
+        }
+
+        [Obsolete("Please use Add(List<int> NumberList) method")]
+        static int Add(int Number1, int Number2)
+        {
+            return Number1 + Number2;
+        }
+
+        static int Add(List<int> NumberList)
+        {
+            int sum = 0;
+            foreach(int number in NumberList)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
