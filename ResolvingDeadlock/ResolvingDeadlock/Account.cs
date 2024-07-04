b1a22c176a81dd48c722cab528788af7f47083ba
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 19:06:19 +0530

    Resolving Deadlock

---
+++ ResolvingDeadlock/ResolvingDeadlock/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolvingDeadlock
+{
+    public class Account
+    {
+        double _balance; int _id;
+        public Account(int id, int balance)
+        {
+            this._balance = balance;
+            this._id = id;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+    }
+}
