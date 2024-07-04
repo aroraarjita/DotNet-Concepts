13e6bce2c5e23247a81a582929d169b2e9da5ddf
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:19:20 +0530

    Intro to Classes

---
+++ IntroductionToClasses/IntroductionToClasses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToClasses
+{
+    class Customer
+    {
+        string _firstName;
+        string _lastName;
+        
+        public Customer():this("Param", "Constructor")
+        {
+
+        }
+
+
+        public Customer(string FirstName, string LastName)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine("Full name is {0}", this._firstName + " " + this._lastName);
+        }
+
+        ~Customer()
+        {
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Customer customer = new Customer("Arjita", "Arora");
+            Customer cust = new Customer();
+            cust.PrintFullName();
+            Console.ReadLine();
+        }
+
+    }
+}
