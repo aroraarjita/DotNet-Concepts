9951aebe54002650b8527321b6a6f5a71c6ca9b7
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:04:15 +0530

    Multiple Inheritance

---
+++ MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A Implementation");
+        }
+    }
+
+    class B: A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B Implementation");
+        }
+
+    }
+
+    class C : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("C Implementation");
+        }
+    }
+
+
+    class D: C, B
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D d = new D();
+            d.Print();
+        }
+    }
+}
