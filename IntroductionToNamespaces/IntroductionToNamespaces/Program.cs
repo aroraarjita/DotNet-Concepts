1d0a49d5780e3d751c62ba4326aee11f8754998b
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:20:23 +0530

    Intro to Namspaces

---
+++ IntroductionToNamespaces/IntroductionToNamespaces/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
+
+
+namespace IntroductionToNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PATA.ClassA.Print();
+            PATB.ClassA.Print();
+            Console.ReadLine();
+ 
+        }
+    }
+}
+
+//namespace ProjectA
+//{
+//    namespace TeamA
+//    {
+//        class ClassA
+//        {
+//            public static void Print()
+//            {
+//                Console.WriteLine("This is Team A");
+//            }
+//        }
+//    }
+
+//}
+
+//namespace ProjectA
+//{
+//    namespace TeamB
+//    {
+//        class ClassA
+//        {
+//            public static void Print()
+//            {
+//                Console.WriteLine("This is Team B!");
+//            }
+
+//        }
+        
+//    }
+//}
