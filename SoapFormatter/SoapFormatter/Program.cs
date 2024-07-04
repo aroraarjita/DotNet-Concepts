04eeb6d9d30b1ae3ddd9e26e17188e7bd58e26e6
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:57:30 +0530

    Soap Formatter

---
+++ SoapFormatter/SoapFormatter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace SoapFormatterSolution
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Serialize()
+        {
+            Hashtable address = new Hashtable();
+            address.Add("Jeff", "123 Main Street, Redmond, WA 98052");
+            address.Add("Fred", "987 Pine Road, Phila., PA 19116");
+            address.Add("Mary", "PO Box 112233, Palo Alto, CA 94301");
+
+            FileStream fs = new FileStream("Datafile.soap", FileMode.Create);
+            SoapFormatter formatter = new SoapFormatterSolution();
+
+        }
+
+
+    }
+}
