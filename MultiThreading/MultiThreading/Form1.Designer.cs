72271daec775cfe90e37f42053031d93c46da705
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:05:10 +0530

    Multithreading

---
+++ MultiThreading/MultiThreading/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace MultiThreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimeConsumingWork = new System.Windows.Forms.Button();
+            this.btnPrintNumbers = new System.Windows.Forms.Button();
+            this.listBoxNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnTimeConsumingWork
+            // 
+            this.btnTimeConsumingWork.Location = new System.Drawing.Point(141, 44);
+            this.btnTimeConsumingWork.Name = "btnTimeConsumingWork";
+            this.btnTimeConsumingWork.Size = new System.Drawing.Size(452, 39);
+            this.btnTimeConsumingWork.TabIndex = 0;
+            this.btnTimeConsumingWork.Text = "Do Time Consuming Work";
+            this.btnTimeConsumingWork.UseVisualStyleBackColor = true;
+            this.btnTimeConsumingWork.Click += new System.EventHandler(this.btnTimeConsumingWork_Click);
+            // 
+            // btnPrintNumbers
+            // 
+            this.btnPrintNumbers.Location = new System.Drawing.Point(141, 129);
+            this.btnPrintNumbers.Name = "btnPrintNumbers";
+            this.btnPrintNumbers.Size = new System.Drawing.Size(452, 38);
+            this.btnPrintNumbers.TabIndex = 1;
+            this.btnPrintNumbers.Text = "Print Numbers";
+            this.btnPrintNumbers.UseVisualStyleBackColor = true;
+            this.btnPrintNumbers.Click += new System.EventHandler(this.btnPrintNumbers_Click);
+            // 
+            // listBoxNumbers
+            // 
+            this.listBoxNumbers.FormattingEnabled = true;
+            this.listBoxNumbers.ItemHeight = 20;
+            this.listBoxNumbers.Location = new System.Drawing.Point(141, 233);
+            this.listBoxNumbers.Name = "listBoxNumbers";
+            this.listBoxNumbers.Size = new System.Drawing.Size(452, 184);
+            this.listBoxNumbers.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxNumbers);
+            this.Controls.Add(this.btnPrintNumbers);
+            this.Controls.Add(this.btnTimeConsumingWork);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimeConsumingWork;
+        private System.Windows.Forms.Button btnPrintNumbers;
+        private System.Windows.Forms.ListBox listBoxNumbers;
+    }
+}
+
