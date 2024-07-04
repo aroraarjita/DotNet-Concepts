ba30a527652455c46b87bed254c7aadf8d0a5ed0
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:20:11 +0530

    Difference b/w String And Builder

---
+++ DiffBWStringAndBuilder/DiffBWStringAndBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffBWStringAndBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usrString = "C# ";
+            usrString += "Video ";
+            usrString += "Tutorial ";
+            usrString += "for ";
+            usrString += "Beginners";
+
+            Console.WriteLine(usrString);
+
+
+            StringBuilder userString = new StringBuilder();
+            userString.Append("C# ");
+            userString.Append("Tutorial ");
+            userString.Append("for ");
+            userString.Append("Beginners");
+
+            Console.WriteLine(userString);
+
+            Console.ReadLine();
+        }
+    }
+}
