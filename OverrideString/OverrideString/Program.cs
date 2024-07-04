f1111bb97cd86ddee29b1589fd2e1400025876f9
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 19:02:38 +0530

    Override String

---
+++ OverrideString/OverrideString/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer();
+            C1.FirstName = "Arjita";
+            C1.LastName = "Arora";
+
+            Customer C2 = new Customer();
+            C2.FirstName = "Arjita";
+            C2.LastName = "Arora";
+
+            Console.WriteLine(C1 == C2);
+            Console.WriteLine(C1.Equals(C2));
+
+            Console.WriteLine(C1.GetHashCode());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+
+            if(!(obj is Customer))
+            {
+                return false;
+            }
+
+            return this.FirstName == ((Customer)obj).FirstName &&
+                this.LastName == ((Customer)obj).LastName;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();
+
+        }
+
+
+    }
+}
---
+++ OverrideToString/OverrideToString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Arjita";
+            customer.LastName = "Arora";
+
+            Console.WriteLine(customer.ToString());
+            Console.ReadLine();
+        }
+    }
+
+     public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+    }
+}
