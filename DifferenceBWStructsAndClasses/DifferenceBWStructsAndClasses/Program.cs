3c08ec819f0ac2a1c7a046755ba2c10962f462ff
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:18:08 +0530

    Diff in Strcut and Classes

---
+++ DifferenceBWStructsAndClasses/DifferenceBWStructsAndClasses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferenceBWStructsAndClasses
+{
+    interface ICusomer1
+    {
+        void Print1();
+    }
+
+    interface ICustomer2 : ICusomer1
+    {
+        void Print2();
+    }
+
+ 
+
+
+
+
+
+    public class Customer: ICustomer2
+    {
+   
+       public void Print2()
+        {
+            Console.WriteLine("This is Print2");
+        }
+
+        public void Print1()
+        {
+            Console.WriteLine("This is Print1");
+        }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICusomer1 customer = new Customer();
+            customer.Print1();
+         
+
+
+
+        }
+    }
+}
