e9752d98182c2f12e6b87c35fff6d6169889d046
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 16:56:22 +0530

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void HelloFunctionDelegate(string message);
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            HelloFunctionDelegate functionDelegate = new HelloFunctionDelegate(Hello);
+            functionDelegate("Hello from delegate");
+            Console.ReadLine();
+
+        }
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
