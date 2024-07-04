e712508683ea7c351fe5fb72d292691eafc7b33b
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:37:46 +0530

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandling
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"C:\Sample Files\Data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+                Console.ReadLine();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadLine();
+
+                Console.WriteLine("Please check if the file {0} exists", ex.FileName);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            finally
+            {
+                if(streamReader != null)
+                {
+                    streamReader.Close();
+                }               
+                Console.WriteLine("Finally Block");
+                Console.ReadLine();
+            }
+           
+
+            
+        }
+    }
+}
---
+++ Indexers/Indexers/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Indexers
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["Session1"] = "Session 1 Data";
+            Session["Session2"] = "Session 2 Data";
+
+            Response.Write("Session 1 = " + Session[0].ToString());
+            Response.Write("<br/>");
+            Response.Write("Session 2 = " + Session["Session2"].ToString());
+
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                SqlCommand command = new SqlCommand("select * from [SchoolDB].[dbo].[Student]",connection);
+                connection.Open();
+                SqlDataReader rdr = command.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Response.Write("Id =  " + rdr[0].ToString() + " ");
+                    Response.Write("Name = " + rdr["StudentName"].ToString());
+                    Response.Write("<br/>");
+                }
+            }
+
+
+        }
+    }
+}
