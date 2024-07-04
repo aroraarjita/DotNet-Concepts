19830ae268d2482799f2a76598b144f5475ebdc3
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 17:07:36 +0530

    Partial Classes 1

---
+++ PartialClasses-1/AddDemo/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AddDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SamplePartialClass obj = new SamplePartialClass();
+
+        }
+    }
+
+    public interface ICustomer
+    {
+       void CustomerMethod();
+    }
+
+    public interface IEmployee
+    {
+        void EmployeeMethod();
+    }
+}
