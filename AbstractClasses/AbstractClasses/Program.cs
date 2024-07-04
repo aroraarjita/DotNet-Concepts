a84b46f82da3493bee3ee4eefd2521df8bfe7636
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 16:46:56 +0530

    Abstract Classes

---
+++ AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    abstract class Customer
+    {
+        int Id;
+        public abstract void Print();
+       
+
+        public  void PrintImplemented()
+        {
+            Console.WriteLine("Print!!");
+        }
+    }
+
+    public interface ICustomer
+    {
+        void  Print();
+    }
+
+    class Program: Customer
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Print Method!");
+        }
+
+        static void Main(string[] args)
+        {
+            //Customer program = new Program();
+            //program.Print();
+            //Console.ReadLine();
+        }
+
+
+    }
+}
