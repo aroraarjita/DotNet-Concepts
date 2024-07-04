db3e903763bdc8720a422a3f51f76c9503e77b2e
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 19:04:00 +0530

    Partial Methods

---
+++ PartialMethodsDemo/PartialMethodsDemo/PartialClassFileOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialMethodsDemo
+{
+    public partial class SamplePartialClass
+    {
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method Invoked");
+            SamplePartialMethod();
+        }
+    }
+}
---
+++ PartialMethodsDemo/PartialMethodsDemo/PartialClassFileTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialMethodsDemo
+{
+    public partial  class SamplePartialClass
+    {
+        partial void SamplePartialMethod();
+        partial void SamplePartialMethod()
+        {
+            Console.WriteLine("SamplePartialMethod Invoked");
+        }
+
+    }
+}
