22e24bcdfaa261b27c9b49717c2a6ac49be43a36
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:03:12 +0530

    Internal protected assembly

---
+++ InternalProtectedAssembly/AssemblyOne/AssemblyOneClassI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    public class AssemblyOneClassI
+    {
+        protected internal int ID = 101;
+    }
+
+
+    public class AssemblyOneClassI1
+    {
+        public void SampleMethod()
+        {
+            AssemblyOneClassI A1 = new AssemblyOneClassI();
+            Console.WriteLine(A1.ID);
+        }
+    }
+}
---
+++ InternalProtectedAssembly/AssemblyTwo/AssemblyTwoClassOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    public class AssemblyTwoClassOne: AssemblyOneClassI
+    {
+        public void Print()
+        {
+            AssemblyOne.AssemblyOneClassI A1 = new AssemblyOneClassI();
+            base.ID = 101;
+
+
+            AssemblyTwoClassOne A2 = new AssemblyTwoClassOne();
+            
+
+        }
+
+
+
+
+
+
+    }
+}
