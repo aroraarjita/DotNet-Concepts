4c95fc0c6d51a260696e6c4a6ecb7941887b7ea1
aroraarjita <a.arjita1601@gmail.com>
2024-07-04 18:10:20 +0530

    Create Indexers

---
+++ CreatingIndexers/CreatingIndexers/Company.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+
+namespace CreatingIndexers
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class Company
+    {
+        List<Employee> listEmployess;
+
+        public Company()
+        {
+            listEmployess = new List<Employee>();
+            listEmployess.Add(new Employee() { EmployeeId = 1, Name = "Mike", Gender = "Male" });
+            listEmployess.Add(new Employee() { EmployeeId = 2, Name = "Pam", Gender = "Female" });
+            listEmployess.Add(new Employee() { EmployeeId = 3, Name = "John", Gender = "Male" });
+            listEmployess.Add(new Employee() { EmployeeId = 4, Name = "Maxine", Gender = "Female" });
+            listEmployess.Add(new Employee() { EmployeeId = 5, Name = "Emiliy", Gender = "Female" });
+            listEmployess.Add(new Employee() { EmployeeId = 6, Name = "Scott", Gender = "Male" });
+            listEmployess.Add(new Employee() { EmployeeId = 7, Name = "Todd", Gender = "Male" });
+            listEmployess.Add(new Employee() { EmployeeId = 8, Name = "Benn", Gender = "Male" });
+        }
+
+
+        public string this[int employeeId]
+        {
+            get
+            {
+                return listEmployess.FirstOrDefault(emp => emp.EmployeeId == employeeId).Name;
+            }
+
+            set
+            {
+                listEmployess.FirstOrDefault(emp => emp.EmployeeId == employeeId).Name = value;
+
+            }
+        }
+
+        public string this[string Gender]
+        {
+            get
+            {
+                return listEmployess.Count(emp => emp.Gender == Gender).ToString();
+            }
+
+            set
+            {
+              foreach(Employee employee in listEmployess)
+                {
+                    if(employee.Gender == Gender)
+                    {
+                        employee.Gender = value;
+                    }
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ CreatingIndexers/CreatingIndexers/WebForm1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CreatingIndexers
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Company company = new Company();
+
+            Response.Write("Before Update");
+            Response.Write("Name of the employee with Id 2 = " + company[2]);
+            Response.Write("<br/>");
+
+            Response.Write("Name of the employee with Id 5 = " + company[5]);
+            Response.Write("<br/>");
+
+
+            Response.Write("Name of the employee with Id 8 = " + company[8]);
+            Response.Write("<br/>");
+
+
+            company[2] = "2nd employee name changed";
+            company[5] = "5th employee name changed";
+            company[8] = "8th employee name changed";
+
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("After Update");
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("Name of the employee with Id 2 = " + company[2]);
+            Response.Write("<br/>");
+
+            Response.Write("Name of the employee with Id 5 = " + company[5]);
+            Response.Write("<br/>");
+
+
+            Response.Write("Name of the employee with Id 8 = " + company[8]);
+            Response.Write("<br/>");
+
+
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+
+            Response.Write("Before Update");
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("Total Male Employees:" + company["Male"]);
+            Response.Write("Total Female Employees:" + company["Female"]);
+
+            company["Male"] = "Female";
+
+
+            Response.Write("After Update");
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("Total Male Employees:" + company["Male"]);
+            Response.Write("Total Female Employees:" + company["Female"]);
+
+
+
+
+
+        }
+    }
+}
