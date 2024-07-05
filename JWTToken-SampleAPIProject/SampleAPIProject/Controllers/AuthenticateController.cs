7068d6b794d189a1bf66c45e6e24373fbbc39d94
aroraarjita <a.arjita1601@gmail.com>
2024-07-05 10:56:54 +0530

    JWT Token Sample Project

---
+++ JWTToken-SampleAPIProject/SampleAPIProject/Controllers/AuthenticateController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleAPIProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+
+namespace SampleAPIProject.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        private IConfiguration _config;
+
+        public AuthenticateController(IConfiguration config)
+        {
+            _config = config;
+        }
+       
+        private string GenerateJSONWebToken(LoginModel userInfo)
+        {
+            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                null,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+        }
+
+
+        private async Task<LoginModel>AuthenticateUser(LoginModel login)
+        {
+            LoginModel user = null;
+
+            if(login.UserName == "Jay")
+            {
+                user = new LoginModel { UserName = "Jay", Password = "123456" };
+            }
+
+            return user;
+        }
+
+        [AllowAnonymous]
+        [HttpPost(nameof(Login))]
+        public async Task<IActionResult> Login([FromBody] LoginModel data)
+        {
+            IActionResult response = Unauthorized();
+            var user = await AuthenticateUser(data);
+                       
+            if(data != null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { Token = tokenString, Message = "Success" });
+            }
+
+            return response;
+
+        }
+
+
+        [HttpGet(nameof(Get))]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            return new string[] { accessToken };
+        }
+
+
+    }
+}
---
+++ JWTToken-SampleAPIProject/SampleAPIProject/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPIProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)]
+    public class BaseController : Controller
+    {
+        
+
+    }
+}
